c0629b84bc5c6fbb925480eea29fd6b740a82dad
Documenter.jl <documenter@juliadocs.github.io>
2024-07-09 06:39:16 +0000

    build based on 69b9ba1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Sole","category":"page"},{"location":"#Sole","page":"Home","title":"Sole","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Welcome to the documentation for Sole, a Julia package for symbolic AI.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Sole]","category":"page"}]
+}
